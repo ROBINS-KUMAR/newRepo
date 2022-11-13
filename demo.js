eb0a331aacea015ec1a2451521a3894ba5b92055
Robins Kumar <robins112002@gmail.com>
2022-11-13 21:41:04 +0530

    firstcommit

---
+++ demo.js
@@ -0,0 +1,44 @@
+// console.dir(document);
+// console.log(document.URL);
+// console.log(document.title);
+// console.log(document.doctype);
+// console.log(document.all);
+// console.log(document.all[10]);
+// console.log(document.forms);
+
+// console.log(document.title);
+
+// console.log(document.getElementById('header-title'));
+// var headerTitle = document.getElementById('header-title');
+// var header =document.getElementById('main-header');
+// // console.log(headerTitle);
+// // headerTitle.textContent = 'HELLO';
+// // headerTitle.innerText = 'Goodbye'  
+//  header.style.borderBottom ='solid 3px #000'
+
+// GETELEMENTSBYCLASSNAME
+// var item =document.getElementsByClassName('list-group-item');
+// console.log(item);
+// console.log(item[2]);
+
+
+// item[2].style.backgroundColor = 'green';
+// for(var i=0; i< item.length; i++)
+// {
+//     item[i].style.fontWeight = 'bold';
+// }
+
+// GETELEMENTSBYTAGNAME
+var li = document.getElementsByTagName('li');
+console.log(li);
+console.log(li[4]);
+li[4].textContent ='Hello';
+
+
+// GETELEMENTSBYCLASSNAME 
+
+
+// var item =document.getElementsByClassName('list-group-item');
+// console.log(item);
+// console.log(item[4]);
+// item[4]
